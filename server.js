0fb5bf12a98d0bbdf8629c13e06a6119983c18f4
rosariowrobel <rosario.wrobel@gmail.com>
2024-12-15 20:27:01 -0300

    First version of the application with Express

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+    res.send('¡Hola mundo! Esta es mi pequeña aplicación desplegada en EC2 con GitHub Actions :)');
+});
+
+app.listen(port, () => {
+    console.log(`Aplicación corriendo en http://localhost:${port}`);
+});
